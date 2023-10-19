826cca1561d9f4cda9b4980fa034d7053d3e7f68
Nepalcem <mark.emailtest@gmail.com>
2023-10-20 02:40:00 +0300

    signup form

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, "Must be at least 3 characters")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Must be at least 8 characters")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Enter your username"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
+        />
+        {formik.touched.username && formik.errors.username ? (
+          <div>{formik.errors.username}</div>
+        ) : null}
+      </div>
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
+      </div>
+
+      <button type="submit">Signup</button>
+    </form>
+  );
+};
+
+export default SignupForm;
--- src/components/pages/Signup/Signup.jsx
+++ src/components/pages/Signup/Signup.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { SignUpMain,SignupImage, FormBlock } from './Signup.styled';
-import SignUpBackground from '../../images/signup-image.jpg';
+import React from "react";
+import { SignUpMain, SignupImage, FormBlock } from "./Signup.styled";
+import SignUpBackground from "../../images/signup-image.jpg";
+import SignupForm from "../../SignupForm/SignupForm";
 
 export default function Signup() {
   return (
     <SignUpMain>
       <div className="main-container">
         <SignupImage src={SignUpBackground} alt="hero-background" />
-        <FormBlock><h2 className='login-title'>Register</h2></FormBlock>
+        <FormBlock>
+          <h2 className="login-title">Register</h2>
+          <SignupForm />
+        </FormBlock>
       </div>
     </SignUpMain>
   );
