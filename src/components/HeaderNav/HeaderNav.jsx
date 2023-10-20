206c231353af184892daec15c6e23ff4e500f0ab
Nepalcem <mark.emailtest@gmail.com>
2023-10-20 18:27:38 +0300

    Update HeaderNav.jsx

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { NavButton, NavTag } from "./HeaderNav.styled";
+import { selectIsLoggedIn } from "../redux/selectors";
+import { useSelector, useDispatch } from "react-redux";
+import authOperations from "../redux/auth/authOperations";
 
 export default function HeaderNav() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(authOperations.logOut())
+  }
+
   return (
     <NavTag>
       <NavButton to="/">Home</NavButton>
-      <NavButton to="/login">Login</NavButton>
-      <NavButton to="/signup">Signup</NavButton>
+      {!isLoggedIn ? (
+        <>
+          <NavButton to="/login">Login</NavButton>
+          <NavButton to="/signup">Signup</NavButton>
+        </>
+      ) : (
+        <NavButton to="/" onClick={handleLogout}>
+          Log Out
+        </NavButton>
+      )}
     </NavTag>
   );
 }
